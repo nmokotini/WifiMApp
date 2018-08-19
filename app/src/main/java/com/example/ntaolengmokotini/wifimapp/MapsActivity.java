bd0e59e9fd650c43fca998f2e862920f7e828922
nmokotini <30896445+nmokotini@users.noreply.github.com>
2018-08-19 14:56:00 +0200

    created onStart() method that uses GET and post method

--- app/src/main/java/com/example/ntaolengmokotini/wifimapp/MapsActivity.java
+++ app/src/main/java/com/example/ntaolengmokotini/wifimapp/MapsActivity.java
@@ -10,6 +10,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -29,11 +30,14 @@ import com.google.gson.JsonObject;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
+    private ArrayList get;
 
     /**
      Method called when starting the activity & this is the method where most initialization is done.
@@ -46,8 +50,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-
 
 
     }
@@ -91,6 +93,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+    public void PUT(final double lat, final double lng, final int rssilvl){
+        String URL = "http://196.24.186.131:8080/";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest putRequest = new StringRequest(Request.Method.PUT, URL,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        ) {
+
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("lat", String.valueOf(lat));
+                params.put("long",String.valueOf(lng ));
+                params.put("rssilvl",String.valueOf(rssilvl));
+
+                return params;
+            }
+
+        };
+
+        requestQueue.add(putRequest);
+    }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -102,17 +141,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        ArrayList data = GET();
+        double lat = Double.valueOf(String.valueOf(data.get(0)));
+        double lng = Double.valueOf(String.valueOf(data.get(1)));
+        String rssilvl = (String) data.get(2);
+        LatLng marker = new LatLng(lat,lng);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addMarker(new MarkerOptions().position(marker).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
+    }
+
+    protected void onStart(){
+        super.onStart();
+        ArrayList values = GET();
+        double lat = Double.valueOf(String.valueOf(values.get(0)));
+        double lng = Double.valueOf(String.valueOf(values.get(1)));
+        int rssilvl = getWifiSignalLevel();
+        PUT(lat,lng,rssilvl);
     }
 
     /**
      Calculates SignalLevel of Wifi connection using RSSI values.
      */
-    public String getWifiSignalLevel(){
+    public int getWifiSignalLevel(){
         //gets access to WIFI service through permissions
         WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         //gets information about the wifi connection and stores it in a WifiInfo object
@@ -121,11 +172,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         final int rssiLevels = 5;
         //calulates Signal level
         int wifiSignalLevel = WifiManager.calculateSignalLevel(wifiInfo.getRssi(), rssiLevels);
-        String strWifiSignalLevel = "Wifi Signal Level: "+wifiSignalLevel+"/"+rssiLevels;
-        return strWifiSignalLevel;
+        return wifiSignalLevel;
     }
-
-
-
-
 }
