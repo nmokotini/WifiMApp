8fe9aa7b584e6ba2178c3d3f1daba2e42ba7a818
nmokotini <30896445+nmokotini@users.noreply.github.com>
2018-08-30 07:22:08 +0200

    added methods to retrieve gps data

--- app/src/main/java/com/example/ntaolengmokotini/wifimapp/MapsActivity.java
+++ app/src/main/java/com/example/ntaolengmokotini/wifimapp/MapsActivity.java
@@ -1,9 +1,16 @@
 package com.example.ntaolengmokotini.wifimapp;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
@@ -17,6 +24,19 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -24,8 +44,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import android.location.*;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,13 +60,21 @@ import org.json.JSONObject;
 import java.util.*;
 
 
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    //private ArrayList<Lwdata> mapData;
+
     RequestQueueInstance requestQueueInstance;
     Lwdata post;
+    private static long UPDATE_TIME_INTERVAL = 1000;
+    private static long UPDATE_DISTANCE_INTERVAL = 1;
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+    protected LocationManager locationManager;
+    private LocationRequest locationRequest;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationCallback mLocationCallback;
+    private Location lastLocation;
 
 
     /**
@@ -56,20 +90,142 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
         requestQueueInstance = new RequestQueueInstance(getApplicationContext());
         post = new Lwdata();
-        post.setLat(-33.957748);
-        post.setLng(18.461242);
-        post.setRssilvl(3);
+        /*
+        createLocationRequest();
+        LocationSettingsRequest settingsRequest = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest).build();
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(settingsRequest);
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                int statusCode = ((ApiException) e).getStatusCode();
+                if (statusCode == LocationSettingsStatusCodes.RESOLUTION_REQUIRED) {
+                    // Location settings are not satisfied, but this can
+                    // be fixed by showing the user a dialog
+                    try {
+                        // Show the dialog by calling
+                        // startResolutionForResult(), and check the
+                        // result in onActivityResult()
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(MapsActivity.this, REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error
+                    }
+                }
+            }
+        });
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                for (Location location : locationResult.getLocations()) {
+                    post.setLat(location.getLatitude());
+                    post.setLng(location.getLongitude());
+
+                }
+            }
+        };*/
 
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        getCurrentLocation();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, UPDATE_TIME_INTERVAL, UPDATE_DISTANCE_INTERVAL, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        post.setLat(location.getLatitude());
+                        post.setLng(location.getLongitude());
+                        post.setRssilvl(getWifiSignalLevel());
+                    }
 
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+                        System.out.println("Status Change");
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+
+                        System.out.println("GPS On");
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+                        System.out.println("GPS Off");
+
+                    }
+                }
+        );
     }
+
+
+    private LocationRequest createLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
+    }
+
+    public void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null) {
+            post.setLat(location.getLatitude());
+            post.setLng(location.getLongitude());
+            post.setRssilvl(getWifiSignalLevel());
+        }
+    }
+
+
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMinZoomPreference(10.0f);
+        mMap.setMaxZoomPreference(20.0f);
+        LatLngBounds UPPER_CAMPUS = new LatLngBounds(new LatLng(-33.960347, 18.458184), new LatLng(-33.954616, 18.461242));
+        mMap.setLatLngBoundsForCameraTarget(UPPER_CAMPUS);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UPPER_CAMPUS.getCenter(),17));
+        GET();
+        //POST();
+    }
+
+
     /**
      Requests & obtains values through api
      */
 
     public void GET(){
-        String URL = "http://196.47.207.158:8081/api/v2/lwdata";
-        //mapData.clear();
+        String URL = "http://196.47.203.78:8081/api/v2/lwdata";
         RequestQueue rQueue = requestQueueInstance.getInstance(getApplicationContext()).getRequestQueue();
         //constructing the request
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
@@ -112,12 +268,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     public void POST(){
-        String URL = "http://196.47.207.158:8081/api/v2/lwdata";
+        String URL = "http://196.47.203.78:8081/api/v2/lwdata";
         JSONObject js = new JSONObject();
         try {
-            js.put("lat", -33.957748);
-            js.put("lng", 18.461242);
-            js.put("rssilvl", 3);
+            js.put("lat", post.getLat());
+            js.put("lng", post.getLng());
+            js.put("rssilvl", post.getRssilvl());
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -157,27 +313,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         rQueue.add(jsonObjReq);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.setMinZoomPreference(10.0f);
-        mMap.setMaxZoomPreference(20.0f);
-        LatLngBounds UPPER_CAMPUS = new LatLngBounds(new LatLng(-33.960347, 18.458184), new LatLng(-33.954616, 18.461242));
-        mMap.setLatLngBoundsForCameraTarget(UPPER_CAMPUS);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UPPER_CAMPUS.getCenter(),17));
-        GET();
-        POST();
-    }
-
     /**
      Calculates SignalLevel of Wifi connection using RSSI values.
      */
@@ -192,4 +327,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         int wifiSignalLevel = WifiManager.calculateSignalLevel(wifiInfo.getRssi(), rssiLevels);
         return wifiSignalLevel;
     }
+
+
 }
