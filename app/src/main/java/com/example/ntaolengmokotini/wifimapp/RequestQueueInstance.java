50f5ae3c27ae992068c1f71e4c3c98476e25774f
zzzaJ <dino.bossi4@gmail.com>
2018-09-07 04:15:39 +0200

    Added comments to Request Queue Instance

--- app/src/main/java/com/example/ntaolengmokotini/wifimapp/RequestQueueInstance.java
+++ app/src/main/java/com/example/ntaolengmokotini/wifimapp/RequestQueueInstance.java
@@ -4,6 +4,12 @@ import android.content.Context;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
+/*
+This class allows the creation and utilisation of Request Queue objects.
+Request Queues are utilised by Volley to track and manage requests made to
+an API.
+
+*/
 public class RequestQueueInstance{
 
     private static RequestQueueInstance queueInstance;
